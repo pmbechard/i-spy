55ede5ce922c556fb5cb376fc256fdf54ad1ff3a
Peyton Bechard <pbechard@my.yorku.ca>
2022-10-09 17:53:38 +0800

    Adds images to firebase storage and loads them dynamically based on level

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.scss';
+
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { storage } from '../../firebase/firebase-config';
+import { ref, getDownloadURL } from 'firebase/storage';
 
 interface Props {
   level: number;
 }
 
-const index: React.FC<Props> = ({ level }) => {
-  return <div>Level {level}</div>;
+const Level: React.FC<Props> = ({ level }) => {
+  const [getImg, setImg] = useState<string>('');
+
+  useEffect(() => {
+    const fetchImg = async () => {
+      try {
+        setImg(await getDownloadURL(ref(storage, `level-${level}.jpg`)));
+      } catch (e) {
+        // FIXME: Show loading error
+        console.log(e);
+      }
+    };
+    fetchImg();
+  }, []);
+
+  return (
+    <>
+      <div>Level {level}</div>
+      <img src={getImg} alt='' />
+    </>
+  );
 };
 
-export default index;
+export default Level;
