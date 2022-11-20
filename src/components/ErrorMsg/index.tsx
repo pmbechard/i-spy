2028d584c52dea3dea7b0a4ad53116c5fe92249a
Peyton Bechard <pbechard@my.yorku.ca>
2022-11-20 15:29:08 +0800

    Adds img to ErrorMsg component card

--- src/components/ErrorMsg/index.tsx
+++ src/components/ErrorMsg/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import closeIcon from '../../img/close.png';
+import errorIcon from '../../img/no-connection.png';
 
 interface Props {
   showErrorMsg: boolean;
@@ -14,12 +15,18 @@ const ErrorMsg: React.FC<Props> = ({ showErrorMsg, setShowErrorMsg }) => {
           src={closeIcon}
           alt='close'
           onClick={() => setShowErrorMsg(false)}
+          className='close-icon'
         />
-        <h2>Sorry for the inconvenience...</h2>
-        <p>
-          ...but we've experience an error while connecting to the database.
-          Please check your internet connection and try again.
-        </p>
+        <div className='error-img'>
+          <img src={errorIcon} alt='' />
+        </div>
+        <div className='error-msg'>
+          <h2>Sorry for the inconvenience...</h2>
+          <p>
+            ...but we've experience an error while connecting to the database.
+            Please check your internet connection and try again.
+          </p>
+        </div>
       </div>
     </div>
   ) : (
